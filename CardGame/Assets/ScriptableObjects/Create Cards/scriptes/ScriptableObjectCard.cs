7fb40ea2c3c6d4497ab437c8b4f7a4ebfcea03cb
antoha9117 <44104065+antoha9117@users.noreply.github.com>
2018-12-26 18:18:27 +0300

    1AddCard

--- CardGame/Assets/ScriptableObjects/Create Cards/scriptes/ScriptableObjectCard.cs
+++ CardGame/Assets/ScriptableObjects/Create Cards/scriptes/ScriptableObjectCard.cs
@@ -1,37 +1,90 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System;
+using System.Text.RegularExpressions;
+using System.IO;
+using UnityEditor;
+using System.Linq;
 
 
-[CreateAssetMenu(fileName = "New Card", menuName = "ScriptableObject/Card/Standart", order = 51)]
-public class ScriptableObjectCard : ScriptableObject
+namespace cards
 {
-    public string nameID;
-
-    public string nameCard;
-
-    public string description;
-
-    public int attack;
-
-    public int heals;
-
-    public int manaCost;
-
-    public Sprite artwork;
-
-    
-    public ScriptableObjectCard(string[] value, Sprite in_sprite)
+    [System.Serializable]
+    public class GeneralCardCollection : ScriptableObject
     {
-        this.nameID = value[0];
-        this.nameCard = value[1];
-        this.description = value[2];
-        this.attack = Convert.ToInt32(value[3]);
-        this.heals = Convert.ToInt32(value[4]);
-        this.manaCost = Convert.ToInt32(value[5]);
-        //this.artwork_str = value[6];
-        this.artwork = in_sprite;
+        public List<CardData> value;  // CardData:{ nameID:string, manaCost:int, и тд. }
+
+        public void setlen(int N = 0)
+        {
+            this.value = new List<CardData>();
+            for (int i = 0; i < N; i++)
+                this.value.Add(new CardData());
+        }
+
+        public GeneralCardCollection(string json, string waytosprite = "")
+        {
+            string[] cards = json.Split('}');
+            int N = cards.Length - 2;
+            this.setlen(N);// находим количество карт
+            JsonUtility.FromJsonOverwrite(json, this); //Загружаем list из файла json
+            for (int i = 0; i < N; i++)
+            {
+                this.value[i].LoadSprite(waytosprite);//Загружаем спрайты из resurses
+            }
+        }
+
+        public GeneralCardCollection(int N)
+        {
+            this.setlen(N);
+        }
     }
 
+    [System.Serializable]
+    public class CardData
+    {
+        public string nameID;
+        public string nameCard;
+        public string description;
+        public int attack;
+        public int heals;
+        public int manaCost;
+        public string artwork_name;
+        public Sprite artwork;
+
+        public CardData(string inNameID = "-1", string inNameCard = "default name", string inDescription = "", int inAttack = 0, int inHeals = 1, int inManacost = 100, string inAtwork_name = "default", Sprite inAtwork = null)
+        {
+            this.nameID = inNameID;
+            this.nameCard = inNameCard;
+            this.description = inDescription;
+            this.attack = inAttack;
+            this.heals = inHeals;
+            this.manaCost = inManacost;
+            this.artwork_name = inAtwork_name;
+            this.artwork = inAtwork;
+        }
+
+        public Sprite LoadSprite(string waytosprite)
+        {
+            string nameSprite = "";
+            if (waytosprite == "" || waytosprite == null)
+                nameSprite = this.artwork_name;
+            else
+                nameSprite = waytosprite + "/" + this.artwork_name;
+            if (Resources.Load<Sprite>(nameSprite) == null)//Проверяем что это не спрайт
+            {
+                string path = AssetDatabase.GetAssetPath(Resources.Load(nameSprite));// Здесь не совсем по феншую
+                //Debug.Log(path);
+                AssetDatabase.ImportAsset(path);
+                TextureImporter importer = AssetImporter.GetAtPath(path) as TextureImporter;// делаем из картинки-ассета спрайт
+                importer.textureType = TextureImporterType.Sprite;
+                AssetDatabase.WriteImportSettingsIfDirty(path);
+                AssetDatabase.Refresh();
+                Debug.Log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! reload sprite " + path);
+            }
+
+            this.artwork = Resources.Load<Sprite>(nameSprite);
+
+            return this.artwork;
+        }
+    }
 }
