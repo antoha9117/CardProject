8078a3be6ce633c0c2ffa597aa99cb598e9b5667
antoha9117 <44104065+antoha9117@users.noreply.github.com>
2018-12-31 15:32:10 +0300

    add death card shader

---
+++ CardGame/Assets/Graphics/Materials/death/example_deat.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class example_deat : MonoBehaviour {
+
+    public Material izmmat;
+    public float i = 1;
+    public float v = 0.001f;
+
+    // Use this for initialization
+    void Start()
+    {
+        v = 0.5f;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (i > 0)
+            i -= Time.deltaTime * v;
+        else
+            i = 1;
+        izmmat.SetFloat("Vector1_EA99F36D", i);
+    }
+}
